dfe7d9c023c1dc5fe2cd7dfd7e3e1a1da1b885a0
Molnár Bálint <balint20010113@gmail.com>
2024-12-16 11:54:22 +0100

    feat: ManageAccount.tsx structure refactor

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -3,7 +3,6 @@ import {Navbar} from "../Navbar/Navbar.tsx";
 import {Menu} from "../Menu/Menu.tsx";
 import {useAuth} from "../../hooks/auth.ts";
 import {ScrollTopButton} from "../ScrollTopBtn/ScrollTopButton.tsx";
-import { ToastContainer } from 'react-toastify';
 
 
 
@@ -21,7 +20,7 @@ const Layout = () => {
             </div>
         </div>
         <ScrollTopButton/>
-        <ToastContainer position={"bottom-right"} autoClose={5000} newestOnTop={true} theme={"dark"}/>
+
     </>
 }
 
--- client/src/pages/ManageAccount/ManageAccount.tsx
+++ client/src/pages/ManageAccount/ManageAccount.tsx
@@ -1,10 +1,8 @@
 import {useAppSelector} from "../../store/hooks.ts";
-import {Badge} from 'primereact/badge';
 import {ManageFollowers} from "./components/ManageFollowers/ManageFollowers.tsx";
 import {ManagePosts} from "./components/ManagePosts/ManagePosts.tsx";
 import {AccountInformation} from "./components/AccountInformation/AccountInformation.tsx";
 import {useManageAccount} from "./hooks/useManageAccount.tsx";
-import {AccountAvatar} from "../../components/AccountAvatar/AccountAvatar.tsx";
 
 
 const ManageAccount = () => {
@@ -17,19 +15,6 @@ const ManageAccount = () => {
 
     return <>
         <div className={"w-full h-full flex flex-col items-center"}>
-            <div className={"flex items-center gap-10 w-full"}>
-                <AccountAvatar username={user?.username || ""} size={"xlarge"} color={""}/>
-                <div>
-                    <div className="text-2xl font-semibold">{user?.username}</div>
-                    <div>{user?.email}</div>
-                    <div className={"flex gap-2 mt-5"}>
-                        <Badge value={"10 followers"}/>
-                        <Badge value={"30 shared posts"} severity={"secondary"}/>
-                    </div>
-                </div>
-            </div>
-
-
             <AccountInformation/>
             <ManageFollowers/>
             <ManagePosts/>

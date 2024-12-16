d2bd9e7ebc96e1b95a9cf48326ef45b428f46479
Molnár Bálint <balint20010113@gmail.com>
2024-12-16 11:51:08 +0100

    feat: FollowerCard.tsx refactored, add remove, reject and accept follow request button handlers

--- client/src/pages/ManageAccount/components/ManageFollowers/FollowerCard.tsx
+++ client/src/pages/ManageAccount/components/ManageFollowers/FollowerCard.tsx
@@ -1,27 +1,84 @@
-import {memo} from "react";
+import {memo, useCallback} from "react";
 import {Button} from "primereact/button";
+import {AccountAvatar} from "../../../../components/AccountAvatar/AccountAvatar.tsx";
+import {getApi} from "../../../../config/api.ts";
+import {toast} from "react-toastify";
 
 
 type FollowerCardProps = {
-    data:any
+    data:any,
+    setFollowers:any
 }
 
-export const FollowerCard = memo(() =>{
+export const FollowerCard = memo(({data, setFollowers}:FollowerCardProps) =>{
 
+    const acceptFollow = useCallback(async () =>{
+        try{
+            const response = await getApi().put(`follow/accept/${data._id}`);
+
+            setFollowers((prevState:any)=>{
+                return prevState.map((e)=>{
+                    if (e._id === response.data._id){
+                        return {...e, status:"accepted"}
+                    }
+
+                    return e;
+                })
+            })
+            toast.success("You accepted the follow request. ")
+        }
+        catch (e){
+            toast.error("Something went wrong.")
+        }
+
+    },[data._id])
+
+
+    const rejectFollow = useCallback(async () =>{
+        try{
+            const response = await getApi().delete(`follow/reject/${data._id}`);
+            setFollowers((prevState:any)=>{
+               return prevState.filter((x)=>x._id !== response.data);
+            })
+
+            toast.info("You rejected the follow request. ");
+        }
+        catch (e){
+            toast.error("Something went wrong.")
+        }
+    },[data._id]);
+
+
+
+    const removeFollow = useCallback(async () =>{
+        try{
+            const response = await getApi().delete(`follow/reject/${data._id}`);
+            setFollowers((prevState:any)=>{
+                return prevState.filter((x)=>x._id !== response.data);
+            })
+
+            toast.info("You deleted the follow.");
+        }
+        catch (e){
+            toast.error("Something went wrong.")
+        }
+    },[data._id]);
 
     return <div className={"flex items-center justify-between w-full"}>
         <div className={"flex items-center gap-2"}>
-            <img
-                src={"https://megaport.hu/media/king-include/uploads/2023/10/906363-female-avatar-profile-picture-013.jpg"}
-                className={"rounded-full"} width={48}/>
+            <AccountAvatar username={data.followerId.username} size={"small"}/>
             <div>
-                <div className={"font-semibold"}>test1</div>
-                <div>test1@gmail.com</div>
+                <div className={"font-semibold"}>{data.followerId.username}</div>
+                <div>{data.followerId.email}</div>
             </div>
         </div>
 
+        <div className={"flex items-center"}>
+            <Button icon={"pi pi-check"} severity={"success"} text tooltip={"Accept follow request"} tooltipOptions={{position:"bottom"}} onClick={acceptFollow} visible={data.status !== "accepted"}/>
+            <Button icon={"pi pi-times"} severity={"danger"} text tooltip={"Reject follow request"} tooltipOptions={{position:"bottom"}} onClick={rejectFollow} visible={data.status === "pending"}/>
+            <Button icon={"pi pi-ban"} severity={"danger"} text tooltip={"Remove follower"} tooltipOptions={{position:"bottom"}} onClick={removeFollow} visible={data.status !== "pending"}/>
+        </div>
 
-        <Button icon={"pi pi-ban"} severity={"danger"} text tooltip={"Remove follower"} tooltipOptions={{position:"bottom"}}/>
     </div>
 
 

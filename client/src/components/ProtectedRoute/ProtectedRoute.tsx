22f06d91bba16484c0bf2648246fe8e3f042dc64
Molnár Bálint <balint20010113@gmail.com>
2024-12-16 11:49:59 +0100

    refactor: ProtectedRoute.tsx: Redux stored user value changed to token to redirecting to login page if not authenticated

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,16 +1,17 @@
-import {useAppSelector} from "../../store/hooks.ts";
-import {
-    Outlet,
-
-} from "react-router-dom";
+import {Outlet,} from "react-router-dom";
 import {routes} from "../../config/routes.ts";
+import {useCookies} from "react-cookie";
+import {useEffect} from "react";
 
 export const ProtectedRoute = () =>{
 
-    const user = useAppSelector(state=>state.auth.user);
+    const [token] = useCookies(["AUTH_TOKEN"]);
 
 
-    if (!user) window.location.href = routes.signin;
+    useEffect(()=>{
+        if (!token) window.location.href = routes.signin;
+    },[token])
+
 
 
     return <Outlet/>

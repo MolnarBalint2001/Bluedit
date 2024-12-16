08e223efdc0578b935251340125671d9714d77f6
Molnár Bálint <balint20010113@gmail.com>
2024-12-16 11:42:41 +0100

    refactor: Navbar.tsx refactored, user info changed

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ import {OverlayPanel} from "primereact/overlaypanel";
 import {UserProfile} from "../UserProfile/UserProfile.tsx";
 
 
-export const Navbar = memo(() => {
+export const Navbar = () => {
 
 
     //Ref
@@ -21,22 +21,18 @@ export const Navbar = memo(() => {
     // Navigation
     const navigate = useNavigate();
 
-    //Redux
-    const dispatch = useAppDispatch();
 
+    console.log(user);
 
     return <div className={"flex items-center px-6 py-4 justify-between fixed w-full h-[60px] z-50 shadow-sm"}>
         <div className={"flex gap-2"}>
             <img src={Logo} alt={"Logo"} onClick={()=>navigate(routes.posts)} className={"cursor-pointer"}/>
         </div>
         <div className={"flex gap-2"}>
-
-            <Button visible={user === null} icon="pi pi-sign-in" iconPos={"right"} label={"Sign in"} size={"small"} onClick={()=>navigate(routes.signin)}/>
-            <Button visible={user === null} icon="pi pi-user-plus" iconPos={"right"} label={"Sign up"} text={true} size={"small"} onClick={()=>navigate(routes.signup)}/>
-            <Avatar label={user?.username?.slice(0,2).toUpperCase()} size="normal" shape="circle" onClick={(e)=>userProfileRef.current?.toggle(e)}/>
+            <Avatar label={user?.username?.slice(0,2).toUpperCase()} size="normal" style={{background:user?.profileColor || ""}} shape="circle" onClick={(e)=>userProfileRef.current?.toggle(e)}/>
             <UserProfile ref={userProfileRef}/>
         </div>
     </div>
 
 
-})
+}
